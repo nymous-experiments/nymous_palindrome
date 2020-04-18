0791e8ebef77d298afb9fa4324f51c7b36e610b0
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2020-04-18 14:01:08 +0200

    First commit

---
+++ lib/nymous_palindrome.rb
@@ -0,0 +1,6 @@
+require "nymous_palindrome/version"
+
+module NymousPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nymous_palindrome/version.rb
@@ -0,0 +1,3 @@
+module NymousPalindrome
+  VERSION = "0.1.0"
+end
