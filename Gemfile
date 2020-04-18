52e5c375451d18411271a18160658dad33d27793
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2020-04-18 16:26:29 +0200

    feat: add String#palindrome? function

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gemspec
 gem "rake", "~> 12.0"
 gem "minitest", "~> 5.0"
 gem "minitest-reporters", "~> 1.4"
+gem "yard", "~> 0.9.24"
--- lib/nymous_palindrome.rb
+++ lib/nymous_palindrome.rb
@@ -1,6 +1,18 @@
 require "nymous_palindrome/version"
 
-module NymousPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Test if a String is a palindrome (ie it reads the same from left to right and from right to left)
+  # @example
+  #   "not a palindrome".palindrome? #=> false
+  #   "Racecar".palindrome? #=> true
+  # @return [Boolean]
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+    def processed_content
+      self.scan(/[[:alpha:]]/i).join.downcase
+    end
 end
